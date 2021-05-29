00e923278005ce23c7541d617d64b26164317314
AMimuro <amimuro0120@gmail.com>
2021-05-29 15:59:11 +0900

    ログイン画面作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :"sessions/new"
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday).merge(user_id:current_user.id)
+  end
+
 end
