ecd83029ce8e8740690c01454788932fcc0a8d17
AMimuro <amimuro0120@gmail.com>
2021-06-05 16:10:37 +0900

    userモデル単体テストコード修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,21 +12,17 @@ RSpec.describe @user, type: :model do
         expect(@user).to be_valid
       end
       
+      
+    end
+    
+    context '新規登録できないとき' do
+      
       it 'nicknameが空では登録できない' do
         @user.nickname =''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-
-      it 'passwordが6文字以上であれば登録できること' do
-        @user.password = 'aaa000'
-        @user.password_confirmation = 'aaa000'
-        expect(@user).to be_valid
-      end
-    end
-
-    context '新規登録できないとき' do
-
+      
       it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
