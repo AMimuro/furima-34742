c03395607a53504a2970e33862dcf10c173b5807
AMimuro <amimuro0120@gmail.com>
2021-06-05 19:05:17 +0900

    itemsテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,24 +2,24 @@ class ItemsController < ApplicationController
   def index
   end
 
-  #def new
-    #@item = Item.new
- # end
+  def new
+    @item = Item.new
+  end
 
- # def create
-   # @item = Item.new(item_params)
-   # if @item.save
-      #redirect_to root_path
-    #else
-      #render :"sessions/new"
-    #end
-  #end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render "items/new"
+    end
+  end
 
 
-  #private
+  private
 
-  #def item_params
-    #params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday).merge(user_id:current_user.id)
-  #end
+  def item_params
+    params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday).merge(user_id:current_user.id)
+  end
 
 end
