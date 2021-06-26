da58f54b27e6eb6e4c9ae54aa0819f68479486f1
AMimuro <amimuro0120@gmail.com>
2021-06-26 17:35:10 +0900

    商品購入ページへの遷移、orderファイルの削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,23 @@
 class PurchasesController < ApplicationController
 
   def index
-
+    #@purchase = Purchase.new
   end
 
   def create
-
+    #@purchase = Purchase.new(purchase_params)
+    #if @purchase.valid?
+      #@purchase.seve
+      #return redirect_to root_path
+    #else
+      #render 'index'
+    #end
   end
-  
+
+  private
+
+  #def purchase_params
+    #params.require(:purchase).permit(:postal_code, :ship_from_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  #end
+
 end
