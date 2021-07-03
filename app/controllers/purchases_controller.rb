40a3bdca4696a28e04fc2847b7b76f0daae266af
AMimuro <amimuro0120@gmail.com>
2021-07-03 17:02:09 +0900

    配送先の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,25 +1,25 @@
 class PurchasesController < ApplicationController
 
   def index
-    @purchase = Purchase.new
+    @purchase_receiver = PurchaseReceiver.new
     @item = Item.find(params[:item_id])
   end
+  
 
   def create
-    @purchase = Purchase.new(purchase_params)
-    binding.pry
-    if @purchase.valid?
-      @purchase.seve
-      return redirect_to root_path
+    @purchase_receiver = PurchaseReceiver.new(purchase_params)
+    if @purchase_receiver.valid?
+       @purchase_receiver.save
+       redirect_to root_path
     else
-      render 'index'
+       render action: :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase).permit(:postal_code, :ship_from_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_receiver).permit(:postal_code, :ship_from_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
---
+++ app/models/purchase_receiver.rb
@@ -0,0 +1,17 @@
+class PurchaseReceiver
+  include ActiveModel::Model
+  attr_accessor :postal_code, :ship_from_id, :city, :address, :building_name, :phone_number, :purchase, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :address
+    validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+  end
+  validates :ship_from_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+  purchase =  Purchase.create(user_id: user_id, item_id: item_id)
+  Receiver.create(postal_code: postal_code, ship_from_id: ship_from_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase: purchase)
+  end
+end
